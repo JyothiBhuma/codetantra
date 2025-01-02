2fb7955f589472d2f9fbd802268ff1e4d77bd1db
JyothiBhuma <155645838+JyothiBhuma@users.noreply.github.com>
2025-01-02 14:24:18 +0530

    Create bookingRoutes.js

---
+++ bookingRoutes.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const Booking = require('../models/Booking');
+
+const router = express.Router();
+
+router.post('/', async (req, res) => {
+    try {
+        const newBooking = new Booking(req.body);
+        const savedBooking = await newBooking.save();
+        res.status(201).json(savedBooking);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+
+router.get('/', async (req, res) => {
+    try {
+        const bookings = await Booking.find();
+        res.json(bookings);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+module.exports = router;
