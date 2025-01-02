abf6efbea88c83698a71f36de0dadb3f7ba66824
JyothiBhuma <155645838+JyothiBhuma@users.noreply.github.com>
2025-01-02 14:28:47 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+import BookingForm from '../components/BookingForm';
+
+const Home = () => (
+    <div>
+        <h1>Restaurant Table Booking</h1>
+        <BookingForm />
+    </div>
+);
+
+export default Home;
